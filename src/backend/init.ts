a960b43aded22a161cbbbb1c6a427ac82ff23334
calebtuttle <1calebtuttle@gmail.com>
2023-01-07 14:51:14 -0500

    Fix incorrect url path for db

--- src/backend/init.ts
+++ src/backend/init.ts
@@ -8,7 +8,8 @@ dotenv.config();
 // robust database in production.
 async function initializeDatabase() {
   const __dirname = dirname(fileURLToPath(import.meta.url));
-  const sqlDbPath = process.env.PATH_TO_SQLITE_DB ?? `${__dirname}/../../db.sqlite3`;
+  const sqlDbPath =
+    process.env.PATH_TO_SQLITE_DB ?? `${__dirname}/../../database/db.sqlite3`;
   const sqlDb = new sqlite3.Database(sqlDbPath);
   await new Promise<void>((resolve) => {
     sqlDb.serialize(() => {
